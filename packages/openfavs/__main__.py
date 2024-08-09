1fe09b458c14e459b9bc01bdd112b46d5deb5c78
Tuo Nome <double.facessss@gmail.com>
2024-08-09 18:23:56 +0200

    test basic rest api endpoint

--- packages/openfavs/__main__.py
+++ packages/openfavs/__main__.py
@@ -1,13 +1,19 @@
 #--web true
 #--kind python:default
 
+import json
+
 #import doc_assistant
+
 
 def main(args):
     print('hello from main')
     name = args.get("name", "world")
     greeting = "Hello " + name + "!"
-    return {"body": greeting}
+    # return {"body": greeting}
+    response = {"body": greeting}
+    # Serializza il dizionario in una stringa JSON
+    return json.dumps(response)
     """
     return { 
         "body": doc_assistant.main(args)
