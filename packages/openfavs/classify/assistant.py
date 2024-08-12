e5e8896628dfe77b2fb73ca0049b99c5ca8941fd
Tuo Nome <double.facessss@gmail.com>
2024-08-12 16:49:21 +0100

    testing new assistant functions

--- packages/openfavs/classify/assistant.py
+++ packages/openfavs/classify/assistant.py
@@ -12,7 +12,48 @@ class Config:
     OUT_OF_SERVICE = ""
     INAPPROPRIATE = ""
 
+import re, json, os
+import requests
+from openai import AzureOpenAI, BadRequestError
+#from bs4 import BeautifulSoup
+#import traceback, random
+#from html_sanitizer import Sanitizer
+#from urllib.parse import urlparse
+#from difflib import SequenceMatcher
+
+class ChatBot:
+
+    def __init__(self, args):
+        print('init chatbot()')
+        OPENAI_API_KEY = '89773db3-7863-460c-ad3c-6abd0db43f1c'
+        OPENAI_API_HOST = 'https://openai.nuvolaris.io'
+        print('init chatbot()')        
+        self.key = OPENAI_API_KEY
+        self.host = OPENAI_API_HOST
+        self.ai =  AzureOpenAI(api_version="2023-12-01-preview", 
+                               api_key=self.key, 
+                               azure_endpoint=self.host
+                            )
+        return
+
+class Website:
+
+    def __init__(self):
+        print('init website()')
+        return
+
+AI = None
+Web = None
+
 def main(args):
+
+    global AI, Web
+
+    print('into main')
+
+    if AI is None: AI = ChatBot(args)    
+    if Web is None: Web = Website() 
+
     name = args.get("name", "world")
     greeting = "Hello " + name + "!"
     # return {"body": greeting}
